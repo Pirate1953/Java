8589ef20912efc82dc7c2e16a900fc7204effcc0
Pirate1953 <piratefromnev1953@gmail.com>
2020-09-11 22:58:57 +0300

    Task

---
+++ Task-3/src/ru/mirea/IKBO1719/task3/Main.java
@@ -0,0 +1,11 @@
+package ru.mirea.IKBO1719.task3;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int count = 10;
+	    for (int i = 1; i <= count; i++) {
+	        System.out.print(1.0 / i + " ");
+        }
+    }
+}
